8cac47b931156bcf113aac5dcb7dbe5a79662f41
sekhmet007 <114764032+sekhmet007@users.noreply.github.com>
2023-09-19 09:07:14 +0200

    Update tab_mult.c
    gestion des  nbr negatif

--- exam_rank2/level2/tab_mult.c
+++ exam_rank2/level2/tab_mult.c
@@ -17,10 +17,15 @@ int	ft_atoi(char *str)
 	int res = 0;
 	int i = 0;
 
-	while( str[i] >= '0' && str[i]  <= '9')
-	{
-		res = res * 10 + str[i] - '0';
-		i++;
+	while (str[i])
+	{		
+		if (str[i] >= '0' && str[i]  <= '9')
+		{
+			res = res * 10 + str[i] - '0';
+			i++;
+		}
+		else
+			return (0);
 	}
 	return (res);
 }
@@ -36,14 +41,24 @@ void	tab_mult(int nbr2)
 		write(1, " = ", 3);
 		ft_putnbr(nbr1 * nbr2);
 		write(1, "\n", 1);
+		nb1++;
 	}
 }
 
 int	main(int ac, char **av)
-{
-	if(ac != 2)
+{	
+	int nbr;
+	
+	if (ac != 2)
 		write(1, "\n", 1);
-	else
-		tab_mult(ft_atoi(av[1]));
+	else if (ac == 2)
+	{
+		nbr = ft_atoi(av[1]);
+		
+		if (nbr == 0)
+			return (0);
+		else if (nbr >= 0)
+			tab_mult(nbr);
+	}
 	return (0);
 }
