990c2107373d0fdc5584c18130032e7cc40c89fe
sekhmet007 <114764032+sekhmet007@users.noreply.github.com>
2023-10-03 16:56:50 +0200

    Update ft_rev_wstr.c
    gestion des espaces supplémentaire

--- exam_rank2/level3/ft_rev_wstr.c
+++ exam_rank2/level3/ft_rev_wstr.c
@@ -1,44 +1,83 @@
 /*le programme prend une chaine de caractere en parametre et affiche ses mots dans
-l odre inverse*/
+l odre inverse, il ne doit pas avoir d espace suplementaire*/
 
+
+
+#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-int	main(int ac, char **av)
+char    is_separator(char c)
 {
-	int	i = 0;
-	int	next;
+    return ( c == ' ' || c == '\n' || c == '\t');
+}
+char    **ft_split(char *str)
+{
+    char    **tab;
+    int         mot = 0;
+    int         caractere_mot;
 
-	if (ac == 2)
-	{
-		while (av[1][i])
-			i++;
-		i--;// Revenir au caractere precedent '\0'
+    tab = (char **)malloc(sizeof(char *) * 256);
+    if(tab == NULL)
+        return (NULL);
 
-		while (i >= 0)
-		{
-			if (av[1][i] == ' ')// Si le caractere actuel est un espace, un mot est trouve
-			{
-				next = i + 1;// Boucle pour parcourir le mot inverse a partir du car suivant
-				while (av[1][next] && av[1][next] != ' ')// et tant qu'il dif d 1 espace
-				{
-					write(1, &av[1][next], 1);
-					next++;
-				}
-				write(1, " ", 1);
-			}
-			else if (i == 0)// Ou si le debut de la chaine est atteint
-			{
-				next = i;// Boucle pour parcourir le mot inverse a partir du car actuel
-				while (av[1][next] && av[1][next] != ' ')// et tant qu'il dif d 1 espace
-				{
-					write(1, &av[1][next], 1);
-					next++;
-				}
-			}
-			// Passer au mot precedent
-			i--;
-		}
-	}
-	write(1, "\n", 1);
-	return (0);
+    while (*str)
+    {
+        while(is_separator (*str))
+            str++;
+
+        if(*str == '\0')
+            break;
+
+        caractere_mot = 0;
+
+        tab[mot] = (char *)malloc(sizeof(char) * 4096);
+        if (tab[mot] == NULL)
+            return (NULL);
+
+        while(*str && !is_separator(*str))
+            tab[mot][caractere_mot++] = *str++;
+       // tab[mot][caractere_mot] = '\0';
+        mot++;
+    }
+    tab[mot] = NULL;
+    return (tab);
+    }
+
+int main(int ac, char **av)
+{
+    int i = 0;
+
+    if (ac == 2)
+    {
+        char **words = ft_split(av[1]); // Split de la string
+        if (words == NULL)
+          return 1;
+
+        // trouver le nbr de mots
+        int word_count = 0;
+        while (words[word_count] != NULL)
+            word_count++;
+
+
+        // Reverse and print les mot
+        for (i = word_count - 1; i >= 0; i--)
+        {
+            write(1, words[i], strlen(words[i]));
+            if (i > 0)
+            {
+                write(1, " ", 1);
+            }
+        }
+
+        // Free memory 
+        for (i = 0; i < word_count; i++)
+        {
+            free(words[i]);
+        }
+        free(words);
+    }
+    write(1, "\n", 1);
+    return 0;
 }
