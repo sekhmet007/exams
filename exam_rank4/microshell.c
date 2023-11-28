7bbb98ef4cf526924360c4920b0c2293c20730ff
sekhmet007 <114764032+sekhmet007@users.noreply.github.com>
2023-11-28 16:18:14 +0100

    Update microshell.c

--- exam_rank4/microshell.c
+++ exam_rank4/microshell.c
@@ -12,63 +12,69 @@
 
 #include "microshell.h"
 
-int     msg_err(char * str)
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+int err(char *str)
 {
     while (*str)
         write(2, str++, 1);
-    return (1);
+    return 1;
 }
 
-int     cd(char **av, int nbr_arg)
+int cd(char **av, int i)
 {
-    if (nbr_arg != 2)
-        return msg_err("error: cd: bad arguments\n");
+    if (i != 2)
+        return err("error: cd: bad arguments\n");
     else if (chdir(av[1]) == -1)
-        return msg_err("error: cd :cannot change directory to "), msg_err(av[1]), msg_err("\n");
-    return (0);
+        return err("error: cd: cannot change directory to "), err(av[1]), err("\n");
+    return 0;
 }
 
-int     execute_pipe(char **av, char **envp, int i)
+int exec(char **av, char **envp, int i)
 {
     int fd[2];
     int status;
     int has_pipe = av[i] && !strcmp(av[i], "|");
 
     if (has_pipe && pipe(fd) == -1)
-        return msg_err("error: fatal\n");
-
+        return err("error: fatal\n");
+    
     int pid = fork();
     if (!pid)
     {
         av[i] = 0;
         if(has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
-            return msg_err("error: fatal\n");
+            return err("error fatal\n");
         execve(*av, av, envp);
-        return msg_err("error : cannot execute"), msg_err(*av), msg_err("\n");
+        return err("error: cannot execute "), err(*av), err("\n");
     }
+
     waitpid(pid, &status, 0);
-    if (has_pipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1] == -1)))
-        return msg_err("error: fatal\n");
-    retrun WIFEXITED(status) && WEXITSTATUS(status);
+    if(has_pipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+        return err("error: fatal\n");
+    return WIFEXITED(status) && WEXITSTATUS(status);
 }
 
-int     main(int ac, char **av, char **envp)
+int main(int ac, char **av, char **envp)
 {
     int i = 0;
     int status = 0;
 
-    if(ac > 1)
+    if (ac > 1)
     {
-        while(av[i] && av[++i])
+        while (av[i] && av[++i])
         {
             av += i;
             i = 0;
-            while(av[i] && strcmp(av[i], "|") && strcmp(av[i], ";"))
+            while (av[i] && strcmp(av[i], "|") && strcmp(av[i], ";"))
                 i++;
-            if (!strcmp(*av, "cd"))
+            if(!strcmp(*av, "cd"))
                 status = cd(av, i);
             else if (i)
-                status = execute_pipe(av, envp, i);
+                status = exec(av, envp, i);
         }
     }
     return (status);
